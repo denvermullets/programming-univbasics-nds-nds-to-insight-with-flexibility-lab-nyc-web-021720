974511d12658c3d2a3612810a62b943a6229e078
Ryan Vaznis <ryan.vaznis@gmail.com>
2020-02-08 21:47:35 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -93,6 +93,7 @@ def gross_per_studio(collection)
         puts "does contain studio"
         movie_index += 1
         puts "this is gross #{gross}"
+        puts "this is the list #{list_of_studios}"
       else 
         list_of_studios << collection[movie_index][:studio]
         gross = { collection[movie_index][:studio] => collection[movie_index][:worldwide_gross] }
